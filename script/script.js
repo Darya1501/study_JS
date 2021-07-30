668fd03e156afcb2ae9f71f1fb67fc85f369e7d5
Darya <darya_01@mail.ru>
2021-07-30 12:57:43 +0300

    Добавлены значения переменных и вывод их преобразований

--- script/script.js
+++ script/script.js
@@ -1,4 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 2000;
+let income = 'Иллюстрации';
+let addExpenses = 'Мобильная связь, обучение, проезд';
+let deposit = true;
+let mission = 10000;
+let period = 5;
 
-alert('Текст');
-console.log('Другой текст');
+console.log('money -', typeof(money), ', income -', typeof(income), ', deposit -',  typeof(deposit));
+
+console.log('Длина строки addExpenses:', addExpenses.length);
+
+console.log('Период равен', period, 'месяцев.', 'Цель заработать', mission, 'рублей');
+
+let mass = addExpenses.toLowerCase().split(', ');
+console.log('Дополнительные траты:', mass);
+
+let budgetDay = money / 30;
+console.log('Дневной бюджет:', budgetDay);

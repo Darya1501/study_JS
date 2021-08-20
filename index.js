2e1188f6f7417b0d23fa26394efd32585cc667e2
Darya <darya_01@mail.ru>
2021-08-20 23:11:34 +0300

    Исправлены ошибки в уроке 16

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 // Кнопки управления программой
 const startButton = document.getElementById('start');
@@ -6,12 +6,12 @@ const cancelButton = document.getElementById('cancel');
 
 // Входные данные доходов
 const salary = document.querySelector('.salary-amount');
-let incomeItems = document.querySelectorAll('.income-items');
+const incomeItems = document.querySelectorAll('.income-items');
 const addIncomeButton = document.querySelectorAll('button')[0];
 const addIncomeFields = document.querySelectorAll('.additional_income-item');
 
 // Входные данные расходов
-let expensesItems = document.querySelectorAll('.expenses-items');
+const expensesItems = document.querySelectorAll('.expenses-items');
 const addExpensesButton = document.querySelectorAll('button')[1];
 const addExpensesItem = document.querySelector('.additional_expenses-item');
 
@@ -63,20 +63,23 @@ class AppData {
     this.getInfoDeposit();
     this.getBudget();
     this.getAddExpInc();
-  
+
     this.showResult();
-  
+
     const input = document.querySelectorAll('.data input[type="text"]');
-    input.forEach((item) => {
+    input.forEach(item => {
       item.readOnly = 'true';
       item.style.cursor = 'default';
       item.style.backgroundColor = '#f5efed';
       item.setAttribute('title', 'Сначала сбросте результат');
     });
 
+    depositCheckbox.disabled = true;
+    depositBank.disabled = true;
+
     addIncomeButton.style.display = 'none';
     addExpensesButton.style.display = 'none';
-  
+
     startButton.style.display = 'none';
     cancelButton.style.display = 'block';
   }
@@ -89,7 +92,7 @@ class AppData {
     addIncomeValue.value = this.addIncome.join(', ');
     targetMonthValue.value = this.getTargetMonth();
     incomePeriodValue.value = this.calcSavedMoney();
-  
+
     periodSelect.addEventListener('input', () => {
       incomePeriodValue.value = this.calcSavedMoney();
     });
@@ -97,10 +100,10 @@ class AppData {
 
   reset() {
     const results = document.querySelectorAll('.result-total');
-    results.forEach((item) => {
+    results.forEach(item => {
       item.value = '';
     });
-  
+
     this.budget = 0;
     this.budgetDay = 0;
     this.budgetMonth = 0;
@@ -113,34 +116,37 @@ class AppData {
     this.deposit = false;
     this.percentDeposit = 0;
     this.moneyDeposit = 0;
-  
+
     const input = document.querySelectorAll('.data input[type="text"]');
-    input.forEach((item) => {
+    input.forEach(item => {
       item.removeAttribute('readonly');
       item.style.cursor = 'auto';
       item.removeAttribute('title');
       item.value = '';
       item.style.backgroundColor = '#fff';
     });
-  
+
     periodSelect.value = 1;
     periodAmount.textContent = 1;
-  
+
     expensesItems.forEach((item, i) => {
       if (i !== 0) {
         item.remove();
       }
     });
-  
+
     incomeItems.forEach((item, i) => {
       if (i !== 0) {
         item.remove();
       }
     });
 
+    depositCheckbox.disabled = false;
+    depositBank.disabled = false;
+
     addIncomeButton.style.display = 'block';
     addExpensesButton.style.display = 'block';
-  
+
     startButton.style.display = 'block';
     cancelButton.style.display = 'none';
   }
@@ -149,7 +155,7 @@ class AppData {
   addNewBlock(button) {
     console.log('button: ', button);
     const startStr = button.classList[1].split('_')[0];
-    let items = document.querySelectorAll(`.${startStr}-items`);
+    const items = document.querySelectorAll(`.${startStr}-items`);
 
     const cloneItems = items[0].cloneNode(true);
     cloneItems.childNodes[1].value = '';
@@ -181,7 +187,7 @@ class AppData {
 
   //  Получение списков возможных расходов и доходов
   getAddExpInc() {
-    const write = (item) => {
+    const write = item => {
       let array;
       if (typeof(item) === 'string') {
         item = item.trim();
@@ -197,7 +203,7 @@ class AppData {
     const addExpenses = addExpensesItem.value.split(',');
     addExpenses.forEach(write);
     addIncomeFields.forEach(write);
-    
+
   }
 
   // Вычисление суммы расходов
@@ -221,7 +227,7 @@ class AppData {
 
   // Вычисление накоплений за период
   calcSavedMoney() {
-    return this.budgetMonth * periodSelect.value; 
+    return this.budgetMonth * periodSelect.value;
   }
 
   getInfoDeposit() {
@@ -241,7 +247,7 @@ class AppData {
     const valueSelect = this.value;
     if (valueSelect === 'other') {
       depositPercent.value = '';
-      depositPercent.style.display = 'inline-block';  
+      depositPercent.style.display = 'inline-block';
     } else {
       depositPercent.style.display = 'none';
       depositPercent.value = valueSelect;
@@ -249,7 +255,7 @@ class AppData {
   }
 
   depositHandler() {
-    if(depositCheckbox.checked) {
+    if (depositCheckbox.checked) {
       depositBank.style.display = 'inline-block';
       depositAmount.style.display = 'inline-block';
       this.deposit = true;
@@ -265,22 +271,26 @@ class AppData {
   }
 
   // Слушатели событий
-  eventsListeners() {  
+  eventsListeners() {
     startButton.style.opacity = '0.2';
     startButton.style.cursor = 'default';
     startButton.addEventListener('click', this.start.bind(this));
 
     startButton.addEventListener('mouseenter', () => {
+      if (salary.value.trim() === '') {
+        startButton.style.opacity = '0.2';
+        startButton.style.cursor = 'default';
+        startButton.setAttribute('title', 'Заполните поле "Месячный доход"');
+        startButton.disabled = true;
+      }
+    });
+
+    salary.addEventListener('keydown',  () => {
       if (salary.value.trim() !== '') {
         startButton.style.opacity = '1';
         startButton.style.cursor = 'pointer';
         startButton.removeAttribute('title');
-        startButton.removeAttribute('disable');
-      } else {
-        startButton.style.opacity = '0.2';
-        startButton.style.cursor = 'default';
-        startButton.setAttribute('title', 'Заполните поле "Месячный доход"');
-        startButton.setAttribute('disable', 'true');
+        startButton.disabled = false;
       }
     });
 
@@ -296,21 +306,21 @@ class AppData {
 
   // Валидация полей
   validateField() {
-    const inputs = document.querySelectorAll('input'); 
-    inputs.forEach((field) => {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(field => {
       if (field.getAttribute('placeholder') === 'Наименование' || field.getAttribute('placeholder') === 'название') {
         const validChars = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя.,!? ';
-        field.addEventListener('keydown', (event) => {
+        field.addEventListener('keydown', event => {
           if (validChars.indexOf(event.key) === -1 && event.key !== 'Backspace') {
             event.preventDefault();
-          } 
+          }
         });
       } else if (field.getAttribute('placeholder') === 'Сумма' || field.getAttribute('placeholder') === 'Процент') {
         const validChars = '0123456789';
-        field.addEventListener('keydown', (event) => {
+        field.addEventListener('keydown', event => {
           if (validChars.indexOf(event.key) === -1 && event.key !== 'Backspace') {
             event.preventDefault();
-          } 
+          }
         });
       }
     });
